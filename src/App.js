35a789fe18176414804e0e60b67415d35d4d6cb1
MOHAMMAD NADEEM <mnadeem.mn838@gmail.com>
2023-06-26 15:36:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useEffect } from "react";
+import "./app.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPosts,
+  selectPosts,
+  selectError,
+  selectLoading,
+} from "./redux/postsSlice.js";
+import {
+  Container,
+  Typography,
+  CircularProgress,
+  AppBar,
+  Paper,
+  Grid,
+  List,
+  Button,
+} from "@mui/material";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectPosts);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ mt: "100px" }} className="app">
+      <AppBar
+        sx={{
+          backgroundColor: "#494C13",
+          height: "80px",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h4" align="center" gutterBottom>
+          Demo App With Redux Toolkit & MUI-5
+        </Typography>
+      </AppBar>
+      <Paper
+        sx={{
+          height: "auto",
+          width: "110px",
+          justifyContent: "center",
+          backgroundColor: "blueviolet",
+          marginLeft: "500px",
+        }}
+      >
+        <Typography variant="h4" align="center">
+          Posts
+        </Typography>
+      </Paper>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Typography variant="body1" color="error" align="center">
+          Error: {error}
+        </Typography>
+      ) : (
+        <List>
+          {posts.map((post) => (
+            <Grid container spacing={2} lg={0} direction="column" key={post.id}>
+              {/* ============================================================================================================ */}
+              {/* <Grid item>
+                <Typography variant="h5" color="skyblue" gutterBottom>
+                  User ID: {post.id}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h5" color="black">
+                  {post.title}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">
+                  {post.body}
+                </Typography>
+              </Grid> */}
+              {/* ======================================================================================================================== */}
+              <Grid item>
+                <Typography variant="h5" color="skyblue">
+                  <Button variant="contained">User</Button>- {post.id}
+                </Typography>
+                <Typography variant="h5" color="black">
+                  {post.title}
+                </Typography>
+                <Typography variant="body1">{post.body}</Typography>
+              </Grid>
+            </Grid>
+          ))}
+        </List>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/redux/postsSlice.js
@@ -0,0 +1,42 @@
+// redux/postsSlice.js
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+});
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.posts = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectPosts = (state) => state.posts.posts;
+export const selectLoading = (state) => state.posts.loading;
+export const selectError = (state) => state.posts.error;
+
+export default postsSlice.reducer;
